ff613a2cb4b029b47f9587edb3e8bcc97359e953
ctoestreich <acetrike@yahoo.com>
2013-07-25 12:46:57 -0500

    configuring continuous test build and gh pages publish

--- Gruntfile.js
+++ Gruntfile.js
@@ -285,7 +285,13 @@ module.exports = function (grunt) {
                          karma: {
                              unit: {
                                  configFile: 'karma.conf.js',
-                                 singleRun: true
+                                 singleRun: false,
+                                 autoWatch: true
+                             },
+                             continuous: {
+                                 configFile: 'karma.conf.js',
+                                 singleRun: true,
+                                 browsers: ['PhantomJS']
                              }
                          },
                          cdnify: {
